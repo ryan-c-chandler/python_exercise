70f6f67699c1d1f6e42efea17a785444ea4f6648
Ryan Chandler <ryanchandler0405@yahoo.com>
2023-11-15 14:55:01 -0600

    Update first_prog.py

--- first_prog.py
+++ first_prog.py
@@ -1,2 +1,4 @@
 print("Hello Jeff Grimes, how many screens are we running today?")
 print("Maybe we should run the ball up the middle on 3rd down and long.")
+
+print("No Jeff, that is a bad idea.")
